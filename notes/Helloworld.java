b78129ece355baf5a573ab6e80292d5fd3af86a9
Vishal Shah <vishal.shah@toptal.com>
2023-01-17 16:01:29 +0530

    first commit

---
+++ notes/Helloworld.java
@@ -0,0 +1,10 @@
+
+public class Helloworld {
+
+    public static void main(){
+        System.out.println("Helloworld! Welcome to java");
+        // System.out.println("args counts = " + args.length);
+    }
+}
+
+ 
