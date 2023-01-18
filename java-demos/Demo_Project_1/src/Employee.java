c195ece8539ad8545da6d39d10c49229eb9cb9a6
Vishal Shah <vishal.shah@toptal.com>
2023-01-18 09:49:35 +0530

    -

---
+++ java-demos/Demo_Project_1/src/Employee.java
@@ -0,0 +1,31 @@
+
+public class Employee {
+
+	private  int empID;		//instance variable / data member / properties
+	private String empName;
+	private double salary;
+	
+	public static String companyName;
+	
+	public int getEmpID() {
+		return empID;
+	}
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	} 
+	
+	
+	
+}
